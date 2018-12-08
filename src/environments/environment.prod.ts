767e83f3a14d3ba6839b9e9331a9d51604bd8a11
Renato Nunez <212marines@gmail.com>
2018-12-08 04:43:28 -0400

    hi world

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://bio2-env.2p5bbyxw9r.us-east-2.elasticbeanstalk.com/api"
+  // apiUrl: "http://bio2-env.2p5bbyxw9r.us-east-2.elasticbeanstalk.com/api",
+  apiUrl: "https://lit-mountain-42388.herokuapp.com/api"
   
 };
