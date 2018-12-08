fe9a9e3ac0e5c62c6d8aa79d296db70b203f1e2e
Renato Nunez <212marines@gmail.com>
2018-12-08 17:17:13 -0400

    latest

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var argv = require('yargs').argv;
+var git = require('gulp-git');
+var runSequence = require('run-sequence');
+
+gulp.task('init', function() {
+  console.log(argv.m);
+});
+
+gulp.task('add', function() {
+  console.log('adding...');
+  return gulp.src('.')
+    .pipe(git.add());
+});
+
+gulp.task('commit', function() {
+  console.log('commiting');
+  if (argv.m) {
+    return gulp.src('.')
+      .pipe(git.commit(argv.m));
+  }
+});
+
+gulp.task('push', function(){
+  console.log('pushing...');
+  git.push('origin', 'master', function (err) {
+    if (err) throw err;
+  });
+});
+
+gulp.task('hpush', function(){ 
+  git.push ('heroku', 'master'); 
+});
+
+ 
+
+gulp.task('gitsend', function() {
+  runSequence('add', 'commit', 'hpush');
+});
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from "../../auth/auth.service";
 @Component({
   selector: "app-post-list",
   templateUrl: "./post-list.component.html",
-  styleUrls: ["./post-list.component.css"]
+  styleUrls: ["./post-list.component.scss"]
 })
 export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
