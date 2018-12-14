05a9dde3895699fa04d14870971921a18a52ff52
Renato Nunez <212marines@gmail.com>
2018-12-14 00:14:40 -0400

    l

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   // apiUrl: "http://bio2-env.2p5bbyxw9r.us-east-2.elasticbeanstalk.com/api",
   // apiUrl: "https://lit-mountain-42388.herokuapp.com/api",
-  apiUrl: "http://localhost:3000/api"
+  apiUrl: "http://localhost:3000/api",
+  api_url: "https://lit-mountain-42388.herokuapp.com/api"
   
 };
