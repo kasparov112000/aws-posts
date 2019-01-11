bfaff85562e799ea6c42f8b7aafbb572a2cf01e7
Renato Nunez <212marines@gmail.com>
2019-01-10 17:42:16 -0800

    fixed environment vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   // apiUrl: "http://bio2-env.2p5bbyxw9r.us-east-2.elasticbeanstalk.com/api",
-  // apiUrl: "https://lit-mountain-42388.herokuapp.com/api",
-  //apiUrl: "https://lit-mountain-42388.herokuapp.com/api",
-  api_url: "https://mean-course-back.herokuapp.com/api",
-  apiUrl: "https://mean-course-back.herokuapp.com/api"
+  api_url: "https://lit-mountain-42388.herokuapp.com/api",
+  apiUrl: "https://lit-mountain-42388.herokuapp.com/api",
+  // api_url: "https://mean-course-back.herokuapp.com/api",
+  //  apiUrl: "https://mean-course-back.herokuapp.com/api"
 };
